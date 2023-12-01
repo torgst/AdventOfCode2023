c1e6d9bb11a763d6692fe24c8f268182735a2a2b
Torgeir S <80778551+Gramatus@users.noreply.github.com>
2023-12-01 22:27:04 +0100

    Solution day 1, part 1

--- src/01-1.ts
+++ src/01-1.ts
@@ -1,1 +1,22 @@
-// Ready for the first challenge
+import { readFileSync } from "fs";
+
+const data = readFileSync(`${__dirname}/../data/01-1.txt`, "utf-8");
+
+let sum: number = 0;
+for (const line of data.split("\n")) {
+  if (!line.trim()) continue; // empty line
+  let firstNum: number | null = null;
+  let lastNum: number | null = null;
+  for (const char of line.split("")) {
+    const num = Number.parseInt(char);
+    if (isNaN(num)) continue;
+    if (firstNum === null) firstNum = num;
+    lastNum = num;
+  }
+  if (firstNum === null || lastNum === null) {
+    throw "Should not happen";
+  }
+  const actualNum = Number.parseInt(`${firstNum}${lastNum}`);
+  sum += actualNum;
+}
+console.log(`Sum of calibration values: ${sum}`);
