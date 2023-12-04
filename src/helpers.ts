818ecc05c9c1b4201fd487f8c3dc93cbc88192ff
Torgeir S <80778551+Gramatus@users.noreply.github.com>
2023-12-04 14:18:28 +0100

    Better template and some log helpers

---
+++ src/helpers.ts
@@ -0,0 +1,33 @@
+import { appendFileSync, readFileSync, writeFileSync } from "fs";
+
+export function logJsonPretty(...values: unknown[]) {
+  console.log(values);
+  appendFileSync(
+    `${__dirname}/output.log`,
+    JSON.stringify({ ...values }, null, 2) + "\n",
+  );
+}
+export function logJson(...values: unknown[]) {
+  console.log(values);
+  appendFileSync(
+    `${__dirname}/output.log`,
+    JSON.stringify({ ...values }) + "\n",
+  );
+}
+export function logString(value: string) {
+  console.log(value);
+  appendFileSync(`${__dirname}/output.log`, value + "\n");
+}
+export function handleLines(
+  filename: `${number}${number}-${number}`,
+  handleLine: (line: string) => void,
+  testData?: string,
+) {
+  const data =
+    testData ?? readFileSync(`${__dirname}/../data/${filename}.txt`, "utf-8");
+  writeFileSync(`${__dirname}/output.log`, "");
+  for (const line of data.split("\n")) {
+    if (!line.trim()) continue;
+    handleLine(line);
+  }
+}
--- src/template.ts
+++ src/template.ts
@@ -1,11 +1,13 @@
-import { appendFileSync, readFileSync, writeFileSync } from "fs";
+import { handleLines, logJson } from "./helpers";
 
-const data = readFileSync(`${__dirname}/../data/03-1.txt`, "utf-8");
-
-writeFileSync(`${__dirname}/output.log`, "");
 let sum = 0;
-for (const line of data.split("\n")) {
-  if (!line.trim()) continue;
-  appendFileSync(`${__dirname}/output.log`, JSON.stringify({ sum }) + "\n");
+function handleLine(line: string) {
+  logJson({ sum });
 }
+handleLines(
+  "03-1",
+  handleLine,
+  `
+`,
+);
 console.log("Result: " + sum);
