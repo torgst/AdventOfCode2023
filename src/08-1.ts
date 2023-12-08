63018ed372b488c68a7a5fd02e141ff8b7c11073
Torgeir S <80778551+Gramatus@users.noreply.github.com>
2023-12-08 15:35:56 +0100

    Solution day 8, part 1

--- src/08-1.ts
+++ src/08-1.ts
@@ -1,13 +1,35 @@
-import { handleLines, logJson, logString } from "./helpers";
+import { getData, logJson, logJsonPretty, logString } from "./helpers";
 
-let sum = 0;
-function handleLine(line: string) {
-  logJson({ sum });
-}
-handleLines(
-  "03-1",
-  handleLine,
-  `
-`,
+const data = getData(
+  "08-1",
+  //   `
+  // LLR
+  //
+  // AAA = (BBB, BBB)
+  // BBB = (AAA, ZZZ)
+  // ZZZ = (ZZZ, ZZZ)`,
 );
-logString("Result: " + sum);
+const [instructions, mapInput] = data.trim().split("\n\n");
+type Pointer = { [point: string]: { L: keyof Pointer; R: keyof Pointer } };
+const map: Pointer = {};
+mapInput!.split("\n").forEach((line) => {
+  const [point, targets] = line.split(" = ");
+  if (!targets) throw "Failed to parse";
+  const [L, R] = targets?.replace(/\(|\)/g, "").split(", ");
+  if (!point || !L || !R) throw "Failed to parse";
+  map[point] = { L, R };
+});
+logJsonPretty(map);
+const start = "AAA";
+const end = "ZZZ";
+let current: keyof Pointer = start;
+let stepCount = 0;
+while (current !== end) {
+  for (const instruction of instructions!.trim().split("")) {
+    stepCount++;
+    logJson({ current, instruction });
+    current = map[current]![instruction as "L" | "R"];
+  }
+}
+logJson({ current });
+logString("Result: " + stepCount);
